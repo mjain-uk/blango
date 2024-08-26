e87cfa37c141e7222680f75273a516d128feb3d6
Madhur Jain <madhur.jain.uk@gmail.com>
2024-08-26 15:44:01 +0000

    Finish django configuration

--- blango/wsgi.py
+++ blango/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Set the necessary environment variables
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blango.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
+# Now import and use the configurations' WSGI application
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
