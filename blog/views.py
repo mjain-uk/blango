55bc9d58ce9f4769a4aefaba0a8e8a1bdc2a404b
Madhur Jain <madhur.jain.uk@gmail.com>
2024-08-28 11:43:51 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+import logging
 
+logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
+  logger.info(request)
+  qs = Post.objects.all()
+
+  logger.debug("Got %d posts", qs.count())
   return render(request, 'blog/index.html')
